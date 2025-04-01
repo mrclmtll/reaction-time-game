dc0ef608dd49cc9603352c2bfb5ee78d0b1de06f
mrcl.mtll <marcimatulla@gmail.com>
2025-04-01 21:24:31 +0200

    test: Add failing test for start button click behavior

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from '../App';
+import Game from '../Game';
 
 test('shows title "Reaction Time Game', () => {
   render(<App />);
   const titleElement = screen.getByText(/Reaction Time Game/i);
   expect(titleElement).toBeInTheDocument();
 });
+
+test('shows "start game" button', () => {
+  render(<Game />);
+  const startButton = screen.getByText(/start game/i); // find button to start game
+  fireEvent.click(startButton); // click start-button
+
+  expect(startButton).toBeDisabled(); // check if button disappears
+})
