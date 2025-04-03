31b2b26118d78be1ad7c5de85593b34f48f5f91f
mrcl.mtll <marcimatulla@gmail.com>
2025-04-03 20:51:11 +0200

    refac: rename starting method

--- src/Game.js
+++ src/Game.js
@@ -2,13 +2,13 @@ import { useState } from "react"
 
 function Game () {
 
-    function toggleGame() {
+    function startGame() {
         setGameActive(prev => !prev)
     }
     const [gameActive, setGameActive] = useState(false);
     return (
         <>
-            <button disabled={gameActive} onClick={toggleGame}>Start Game</button>
+            <button disabled={gameActive} onClick={startGame}>Start Game</button>
             {gameActive ? <span>sec</span> : <></>}
             <div style={{borderStyle: "solid", borderWidth: "5px", borderColor:"#101010", height:"50vh", width:"50vw"}} data-testid="gameArea">
 
