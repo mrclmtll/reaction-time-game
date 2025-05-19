1128b8bb4867e372a1dff7f5a4dc322cb8dd23f3
mrcl.mtll <marcimatulla@gmail.com>
2025-05-19 20:41:20 +0200

    feat(order): add 5 buttons at random positions

--- src/gameModes/OrderGame/OrderGame.jsx
+++ src/gameModes/OrderGame/OrderGame.jsx
@@ -1,7 +1,28 @@
 import "./OrderGame.css";
 
 function OrderGame() {
-  return <></>;
+  return (
+    <>
+      {Array.from({ length: 5 }).map((_, i) => {
+        let topPos = Math.random() * 90;
+        let leftPos = Math.random() * 90;
+        return (
+          <TargetButton
+            index={i + 1}
+            style={{ position: "absolute", top: `${topPos}%`, left: `${leftPos}%` }}
+          />
+        );
+      })}
+    </>
+  );
+}
+
+function TargetButton({ style, index }) {
+  return (
+    <button style={style} className="button">
+      {index}
+    </button>
+  );
 }
 
 export default OrderGame;
